ad939f95b6b11e146d71be616cc35b255d78574c
feytrace <feytrace@proton.me>
2026-02-04 19:39:39 -0700

    manual state

--- src/main.rs
+++ src/main.rs
@@ -26,16 +26,16 @@ fn main() {
 
 fn layout_manager(tx: mpsc::Sender<&'static str>) -> Result<(), Box<dyn std::error::Error>> {
     let mut listener = I3EventListener::connect()?;
-    listener.subscribe(&[Subscription::Window])?;
+    listener.subscribe(&[Subscription::{Window, Workspace}])?;
 
-    let mut vertical = true;
+    let mut workspace_state: HashMap<name: Option<String>, is_vertical: bool> = HashMap::new();
+    let mut maybe_current_workspace: Option<String> = Some("1".to_string());
 
     for event in listener.listen() {
         if let Event::WindowEvent(e) = event? {
             if e.change == WindowChange::New || e.change == WindowChange::Close {
-                vertical = !vertical;
-
-                let cmd = if vertical {
+                workspace_state.entry(maybe_current_workspace).and_modify(|is_vertical| *is_vertical = *!is_vertical)
+                let cmd = if workspace_state.entry(maybe_current_workspace) {
                     "split v"
                 } else {
                     "split h"
@@ -43,6 +43,15 @@ fn layout_manager(tx: mpsc::Sender<&'static str>) -> Result<(), Box<dyn std::err
 
                 tx.send(cmd)?;
             }
+        }
+        if let Event::WorkspaceEvent(e) = event? {
+           if e.change == WorkspaceChange::Focus {
+                maybe_current_workspace = WorkspaceEvent::current();
+           } 
+           if e.change == WorkspaceChange::Move {
+                maybe_current_workspace = WorkspaceEvent::current();
+                workspace_state.entry(maybe_current_workspace).and_modify(|is_vertical| *is_vertical = *!is_vertical)
+           }
         }
     }
 
